2fd480ea6f5106343c663554234acc5e10dda1d6
joshuayang <josh.ym3@gmail.com>
2018-04-23 09:34:43 -0700

    Brushed up the code a bit

--- MuSphere/Assets/Scripts/Instantiate512spheres.cs
+++ MuSphere/Assets/Scripts/Instantiate512spheres.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Instantiate512spheres : MonoBehaviour {
-	//smooth movement
-	//public float smoothTime = 0.1F;
-	//private Vector3 velocity = Vector3.zero;
+
+	/** Instantiates 512 spheres that dynamically respond to
+	 * any given audio input. The spheres are broken down
+	 * into eight groups, each representing a range of frequencies.
+	 */
 
 	//holds 8 differnent materials with different colors
 	public Material[] materials;
@@ -27,34 +29,34 @@ public class Instantiate512spheres : MonoBehaviour {
 	// Scales the height of each cube by this much.
 	public float scale;
 	public float shift = 1f;
-	/* Spawns 512 instances of spherePrefab in a "circle" of radius 100
+
+	/* Spawns 512 instances of spherePrefab in a sphere
      * around the object this script is attached to.*/
 	void Start () {
 
-		float radius = 100; //radius of "circle"
+		float radius = 100; //radius of sphere
 
 		for (int i = 0; i < 512; i++) {
+
 			// Spawns a copy of spherePrefab
 			GameObject sphere = Instantiate(spherePrefab);
 
 			// Assigns this copy to it's proper position in cubes.
 			spheres[i] = sphere;
 
-			// Names it properly.
+			// Names it properly with its index.
 			sphere.name = "Sphere" + i;
 
 			// Set its parent to this object.
 			sphere.transform.parent = this.transform;
 
-			/* Rotate and position the cube properly. Some attributes that
-             * might come in handy include Transform.eulerAngles, Transform.forward,
-             * and the Transform class in general. Make sure you're using floats
-             */
+
+			/* Cool stuff to make an interesting spherical pattern of spheres.
+			 * Apparently you get cool patterns when you forget to convert degrees to radians.*/
 			float angle = (360f/512f) * i;
 
 			angles [i] = angle;
 
-			//Apparently you get cool patterns when you forget to convert degrees to radians
 			float xPos = radius * Mathf.Cos(angle);
 
 			float zPos = radius * Mathf.Sin(angle);
@@ -65,16 +67,17 @@ public class Instantiate512spheres : MonoBehaviour {
 
 			sphere.transform.position = new Vector3(shift * xPos, 0,shift * zPos);
 
-			//Set variables for sphere translation (code below)
 			Transform s = transform;
 
+			//Set initial coordinate as a position anchor in case sphere moves
 			initCoord [i] = s.TransformPoint(new Vector3(shift * xPos, 0 ,shift * zPos));
 
 		}
-			
-	}
-		
 
+	}
+
+	/* Updates positions and scales of spheres based
+	 * on the audio input.*/
 	void Update () {
 		for (int i = 0; i < 512; i++) {
 			if (spheres != null) {
@@ -86,6 +89,8 @@ public class Instantiate512spheres : MonoBehaviour {
 				rend.enabled = true;
 
 				int bandNum;
+
+				//Determine which band sphere fits into based on its index.
 
 				if (i <= 64) {
 					bandNum = 0;
@@ -105,13 +110,19 @@ public class Instantiate512spheres : MonoBehaviour {
 					bandNum = 7;
 				}
 
+				//Set band to scale spheres to their respective band sizes
 				band = SpectrumAnalyzer.audioBandBuffers [bandNum];
+				//Set color
 				rend.sharedMaterial = materials[bandNum];
 
 				Transform s = sphere.transform;
 
 				//scale the sphere
-				newScale = 1 + scale * (Mathf.Abs(band));
+				if (i > 448) {
+					newScale = 1.3f + (scale) * (Mathf.Abs(band));
+				} else {
+					newScale = 1 + scale * (Mathf.Abs(band));
+				}
 
 
 				//make sure spheres don't get too big
@@ -122,11 +133,12 @@ public class Instantiate512spheres : MonoBehaviour {
 				//update scale of sphere
 				s.localScale = new Vector3 (newScale, newScale, newScale);
 
-				//move the sphere 
+				//move the sphere based on the new scale
 				Vector3 movementVector = Vector3.Normalize (s.position - transform.position);
-				s.position = initCoord[i] + movementVector * newScale/3f; 
+				s.position = initCoord[i] + movementVector * newScale/2f; 
 
 			}
 		}
 	}
 }
+
